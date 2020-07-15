f8b23b816dd782b9dacead61806ca6f00397b3af
Jan van Hest <janvanhest@outlook.com>
2020-07-15 22:05:20 +0200

    Within #quote-box, I can see a clickable element with a corresponding id=new-quote.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <div id="quote-box">
         <p id="text">"To be or not to be, dobedeee"</p>
         <p id="author">Jan van Hest</p>
+        <button id="new-quote">New Quote</button>
       </div>
     </div>
   );
@@ -16,8 +17,7 @@ function App() {
 ? User Story #1: I can see a wrapper element with a corresponding id="quote-box".
 ? User Story #2: Within #quote-box, I can see an element with a corresponding id="text".
 ? User Story #3: Within #quote-box, I can see an element with a corresponding id="author".
-
-
+? User Story #4: Within #quote-box, I can see a clickable element with a corresponding id="new-quote".
 */
 
 export default App;
