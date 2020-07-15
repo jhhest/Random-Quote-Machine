a59c522a9f2ca38c097e3f814bcbe97de2484258
Jan van Hest <janvanhest@outlook.com>
2020-07-15 02:28:08 +0200

    removed unnecessary css and Within #quote-box, I can see an element with a corresponding id=/text/

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div id="quote-box">Here comes the quote box</div>
+      <div id="quote-box">
+        <p id="text">text</p>
+      </div>
     </div>
   );
 }
 
 /*
 ? User Story #1: I can see a wrapper element with a corresponding id="quote-box".
+? User Story #2: Within #quote-box, I can see an element with a corresponding id="text".
 */
 
 export default App;
